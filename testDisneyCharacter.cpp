f91bd2f33159ec75398a8ccf770405c345046411
Xiao Xue <xxue8761@conestogac.on.ca>
2023-02-03 11:46:36 -0500

    test harness complete

---
+++ testDisneyCharacter.cpp
@@ -0,0 +1,28 @@
+/*
+* File name: testDisneyCharacter.cpp
+* Project name: PROG 1385 - A02 Disney Character
+* By: Xiao Xue
+* First version date: 
+* Description: 
+*/
+
+#include "DisneyCharacter.h"
+
+
+
+int main(void)
+{
+	//instantiate mickey 
+	DisneyCharacter Mickey((char*)"Mickey", (char*)"1929-01-01", 100, 'M');
+	//instantiate minnie 
+	DisneyCharacter Minnie((char*)"Minnie", (char*)"1930-01-01");
+
+	//set minnie's movies same as mickey's 
+	Minnie.SameMovies(Mickey);
+
+	//show information of both characers 
+	Mickey.ShowInfo();
+	Minnie.ShowInfo();
+	//place minnie in Epcot Park 
+	Minnie.PlaceCharacter('E');
+}
